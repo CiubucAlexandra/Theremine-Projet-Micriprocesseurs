7670d9cb468b060135dc5f057b734db970da0f0c
Ciubuc Alexandra <ciubucalexandra@yahoo.com>
2019-01-17 00:06:51 +0200

    primul commit

--- theremine.py
+++ theremine.py
@@ -1,36 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+""" Basic example of retrieving the zx_sensor data, via the I2C interface of
+a Rasberry Pi. Tested with Pi2. When run, prints 'x' & 'z' to console
+"""
+
 
 from __future__ import print_function
 import time
+# project
 from zx_sensor import ZxSensor
-import pygame, numpy, pygame.sndarray
 
 def gotoxy(x,y):
-	print("%c[%d;%df" % (0x1B,y,x), end='')
+    print ("%c[%d;%df" % (0x1B, y, x), end='')
 
-sampleRate=44100
-zx_sensor=ZxSensor(0x10)
-pygame.mixer.pre_init(sampleRate, -16, 1)
-pygame.init()
-while(True):
-#	sampleRate=44100
-	if zx_sensor.position_available():
-		z=zx_sensor.read_z()
-		x=int(zx_sensor.read_x())*44
-		arr=numpy.array([4096*z*40*numpy.sin(2.0*numpy.pi*a/sampleRate)for a in range(0, x)]).astype(numpy.int16)
-		sound=pygame.sndarray.make_sound(arr)
-		sound.play(-1)
-		sound.set_volume(0.5)
-		print('z=')
-		print(z)
-		print('x=')
-		print(x)
-		pygame.time.delay(700)
-		#z_str="{0:03d} ".format(int(z))
-		#print('\033[2J')
-		#gotoxy(int(x/2),int(44-z/8))
-		#print(z_str)
-		sound.stop()
-		#time.sleep(.07)
-		
+# Initialise the ZxSensor device using the default address
+zx_sensor = ZxSensor(0x10)
 
+while (True):
+    if zx_sensor.position_available():
+        # display raw values:
+        # print('x {0} z {1}'.format(zx_sensor.read_x(), zx_sensor.read_z()))
+
+        # display z as console animation:
+        z = zx_sensor.read_z()
+        x = zx_sensor.read_x()
+        z_str = "{0:03d} ".format(int(z))
+        #line = "." * (int(z / 4) + 1)
+        print('\033[2J')
+
+        gotoxy(int(x/2), int(44-z/8))
+        print(z_str)
+
+        time.sleep(.07)
